503764b171d2b178e77e6fbdf2afe9462cd5bb71
Spring MC <Heresy.MC@gmail.com>
2018-03-06 19:02:58 +0800

    What: update build.bat
    Why:
    
      * setup node.exe
    
    How:
    
      * nop

--- build.bat
+++ build.bat
@@ -5,4 +5,9 @@ setlocal
 
 set ELECTRON_MIRROR=http://npm.taobao.org/mirrors/electron/
 
+del ./dist/node/bin/node.bin
+del ./dist/node/bin/node
+
+XCOPY ./node/bin/node.exe ./dist/node/bin/
+
 node node_modules/electron-packager/cli.js ./dist s3-browser --overwrite --out=build --electron-version=1.8.2 --platform=win32 --arch=x64 --icon=./custom/icon.ico 

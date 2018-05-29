0b87ec6f3359d637f1276f1aa26e728f63aba098
Spring MC <Heresy.MC@gmail.com>
2018-05-29 14:17:20 +0800

    What: refactor ioutil package
    Why:
    
      * refactor with s3 sdk
    
    How:
    
      * nop

--- node/s3store/lib/ioutil.js
+++ node/s3store/lib/ioutil.js
@@ -4,18 +4,19 @@ let AWS = require('aws-sdk'),
   EventEmitter = require('events').EventEmitter,
   fs = require('fs'),
   mime = require('mime'),
-  Pend = require('pend');
+  pend = require('pend'),
+  {
+    BufferTransform
+  } = require('./transform');
 
-const MIN_MULTIPART_SIZE = 4 << 20; // 4MB
-const MAX_PUTOBJECT_SIZE = 5 << 30; // 5GB
-const MAX_MULTIPART_COUNT = 10000;
+const {
+  MIN_MULTIPART_SIZE,
+  MAX_PUTOBJECT_SIZE,
+  MAX_MULTIPART_COUNT
+} = require('./consts');
 
 exports.AWS = AWS;
 exports.Client = Client;
-
-exports.MIN_MULTIPART_SIZE = MIN_MULTIPART_SIZE;
-exports.MAX_PUTOBJECT_SIZE = MAX_PUTOBJECT_SIZE;
-exports.MAX_MULTIPART_COUNT = MAX_MULTIPART_COUNT;
 
 exports.createClient = function (options) {
   return new Client(options);
@@ -25,12 +26,12 @@ function Client(options) {
   options = options ? options : {};
 
   this.s3 = options.s3Client || new AWS.S3(options.s3Options);
+  this.s3stream = new BufferTransform({
+    size: options.bufferSize || 4 << 20 // default to 4M
+  });
 
-  this.s3pend = new Pend();
+  this.s3pend = new pend();
   this.s3pend.max = options.maxConcurrency || 10; // connection limit for per domain!
-
-  this.s3RetryCount = options.s3RetryCount || 3;
-  this.s3RetryDelay = options.s3RetryDelay || 1000;
 
   this.resumeUpload = options.resumeUpload === true;
   this.multipartUploadThreshold = options.multipartUploadThreshold || (MIN_MULTIPART_SIZE * 10);
@@ -60,9 +61,6 @@ Client.prototype.uploadFile = function (params) {
   let localFile = params.localFile;
   let localFileStat = null;
 
-  let parts = [];
-  let uploadedParts = {};
-  let checkPoints = params.resumePoints;
   let isOverwrite = params.overwriteDup;
   let isAborted = false;
   let isDebug = params.isDebug;
@@ -70,10 +68,9 @@ Client.prototype.uploadFile = function (params) {
   let uploader = new EventEmitter();
   uploader.setMaxListeners(0);
   uploader.progressLoaded = 0;
-  uploader.progressParts = {};
   uploader.progressTotal = 0;
-  uploader.totalUploadedParts = 0;
-  uploader.totalParts = 0;
+  uploader.progressPartSize = params.uploadedPartSize;
+  uploader.progressParts = params.uploadedParts || null;
   uploader.abort = handleAbort;
 
   params.s3Params.Key = encodeSpecialCharacters(params.s3Params.Key);
@@ -160,13 +157,13 @@ Client.prototype.uploadFile = function (params) {
 
       self.multipartUploadSize = partSize;
 
-      if (self.resumeUpload) {
+      if (self.resumeUpload && self.multipartUploadSize === uploader.progressPartSize) {
         resumeMultipartUpload();
       } else {
         startMultipartUpload();
       }
     } else {
-      doWithRetry(tryPuttingObject, self.s3RetryCount, self.s3RetryDelay, function (err, data) {
+      tryPuttingObject((err, data) => {
         if (isAborted) return;
 
         if (err) {
@@ -180,7 +177,7 @@ Client.prototype.uploadFile = function (params) {
   }
 
   function startMultipartUpload() {
-    doWithRetry(tryMultipartUpload, self.s3RetryCount, self.s3RetryDelay, function (err, data) {
+    tryMultipartUpload((err, data) => {
       if (isAborted) return;
 
       if (err) {
@@ -230,187 +227,7 @@ Client.prototype.uploadFile = function (params) {
   }
 
   function resumeMultipartUpload() {
-    doWithRetry(tryResumeMultipartUpload, self.s3RetryCount, self.s3RetryDelay, function (err, data) {
-      if (isAborted) return;
-
-      if (err) {
-        handleError(err);
-        return;
-      }
-
-      uploader.emit('fileResumedUpload', data);
-
-      uploader.progressLoaded = 0;
-      uploader.totalUploadedParts = 0;
-      for (var i = 0; i < data.Parts.length; i++) {
-        uploadedParts[data.Parts[i].PartNumber] = {};
-        uploadedParts[data.Parts[i].PartNumber].ETag = data.Parts[i].ETag;
-        uploadedParts[data.Parts[i].PartNumber].Size = data.Parts[i].Size;
-        uploadedParts[data.Parts[i].PartNumber].LastModified = data.Parts[i].LastModified;
-
-        uploader.progressLoaded += data.Parts[i].Size;
-        uploader.totalUploadedParts += 1;
-      }
-      uploader.emit('progress', uploader);
-
-      s3params = {
-        Bucket: s3params.Bucket,
-        Key: s3params.Key,
-      };
-
-      makeMultipartUpload(data.UploadId, data.Parts[0] ? data.Parts[0].Size : self.multipartUploadSize);
-    });
-
-    function tryResumeMultipartUpload(cb) {
-      if (isAborted) return;
-
-      if (checkPoints && checkPoints.UploadId) {
-        // list Parts already uploaded
-        self.s3.listParts({
-          Bucket: s3params.Bucket,
-          Key: s3params.Key,
-          UploadId: checkPoints.UploadId
-        }, function (err, data) {
-          if (isAborted) return;
-
-          cb(err, data);
-        });
-      } else {
-        // create new multipart upload
-        self.s3.createMultipartUpload({
-          Bucket: s3params.Bucket,
-          Key: s3params.Key
-        }, function (err, data) {
-          if (isAborted) return;
-
-          data.Parts = [];
-
-          cb(err, data);
-        });
-      }
-    }
-  }
-
-  function makeMultipartUpload(uploadId, partSize) {
-    let cursor = 0;
-    let nextPartNumber = 1;
-    while (cursor < localFileStat.size) {
-      let start = cursor;
-      let end = cursor + partSize;
-      if (end > localFileStat.size) {
-        end = localFileStat.size;
-      }
-      cursor = end;
-
-      let part = {
-        ETag: null,
-        PartNumber: nextPartNumber++
-      };
-
-      parts.push(part);
-
-      uploader.totalParts += 1;
-
-      self.s3pend.go(startUploadPart(start, end, part, uploadId));
-    }
-
-    self.s3pend.wait(function (err) {
-      if (isAborted) return;
-
-      if (err) {
-        handleError(err);
-        return;
-      }
-
-      completeMultipartUpload(uploadId);
-    });
-  }
-
-  function startUploadPart(start, end, part, uploadId) {
-    let retryCount = 0;
-
-    return function (pendCb) {
-      doWithRetry(tryUploadPart, self.s3RetryCount, self.s3RetryDelay, function (err, data) {
-        pendCb();
-
-        if (isAborted) return;
-
-        if (err) {
-          handleError(err);
-          return;
-        }
-
-        uploader.emit('filePartUploaded', data);
-      });
-    };
-
-    function tryUploadPart(cb) {
-      retryCount++;
-
-      if (isAborted) {
-        if (retryCount >= self.s3RetryCount) {
-          cb();
-        }
-        return;
-      }
-
-      if (!!uploadedParts[part.PartNumber]) {
-        part.ETag = uploadedParts[part.PartNumber].ETag;
-
-        cb(null, {
-          Etag: `"${part.Etag}"`,
-          Size: part.Size
-        });
-        return;
-      }
-
-      let params = {};
-      Object.assign(params, s3params);
-
-      params.UploadId = uploadId;
-      params.PartNumber = part.PartNumber;
-      params.ContentLength = end - start;
-      params.Body = fs.createReadStream(localFile, {
-        start: start,
-        end: end
-      });
-
-      let s3uploader = self.s3.uploadPart(params);
-      s3uploader.on('httpUploadProgress', function (prog) {
-        if (isAborted) return;
-
-        let oldLoaded = uploader.progressParts[part.PartNumber] || 0;
-
-        uploader.progressParts[part.PartNumber] = prog.loaded;
-        uploader.progressLoaded += prog.loaded - oldLoaded;
-
-        uploader.emit('progress', uploader);
-      });
-      s3uploader.send(function (err, data) {
-        if (isAborted) return;
-
-        if (err) {
-          cb(err);
-          return;
-        }
-
-        part.ETag = data.ETag;
-
-        uploadedParts[part.PartNumber] = uploadedParts[part.PartNumber] ? uploadedParts[part.PartNumber] : {};
-        uploadedParts[part.PartNumber].ETag = data.ETag;
-        uploadedParts[part.PartNumber].Size = s3params.ContentLength;
-
-        uploader.progressParts[part.PartNumber] = s3params.ContentLength;
-        uploader.totalUploadedParts += 1;
-        uploader.emit('progress', uploader);
-
-        cb(null, data);
-      });
-    };
-  }
-
-  function completeMultipartUpload(uploadId) {
-    doWithRetry(tryCompleteMultipartUpload, self.s3RetryCount, self.s3RetryDelay, function (err, data) {
+    tryResumeMultipartUpload((err, data) => {
       if (isAborted) return;
 
       if (err) {
@@ -421,19 +238,37 @@ Client.prototype.uploadFile = function (params) {
       uploader.emit('fileUploaded', data);
     });
 
-    function tryCompleteMultipartUpload(cb) {
+    function tryResumeMultipartUpload(cb) {
       if (isAborted) return;
 
-      let params = {
-        Bucket: s3params.Bucket,
-        Key: s3params.Key,
-        UploadId: uploadId,
-        MultipartUpload: {
-          Parts: parts
+      angular.forEach(uploader.progressParts, (part, partNumber) => {
+        if (part.ETag !== null) {
+          uploader.progressLoaded += self.multipartUploadSize;
         }
-      };
+      });
+      uploader.emit('progress', uploader);
 
-      self.s3.completeMultipartUpload(params, function (err, data) {
+      var s3uploader = new AWS.S3.ManagedUpload({
+        service: self.s3,
+        params: {
+          Bucket: s3params.Bucket,
+          Key: s3params.Key,
+          Body: fs.createReadStream(localFile)
+        },
+        leavePartsOnError: true,
+        partSize: self.multipartUploadSize,
+        queueSize: self.maxConcurrency
+      });
+      s3uploader.completeInfo = uploader.progressParts;
+
+      s3uploader.on('httpUploadProgress', function (prog) {
+        if (isAborted) return;
+
+        uploader.progressParts = s3uploader.completeInfo;
+        uploader.progressLoaded = prog.loaded;
+        uploader.emit('progress', uploader);
+      });
+      s3uploader.send(function (err, data) {
         if (isAborted) return;
 
         if (err) {
@@ -454,13 +289,13 @@ Client.prototype.uploadFile = function (params) {
     s3params.Body = fs.createReadStream(localFile);
 
     let s3uploader = self.s3.putObject(s3params);
-    s3uploader.on('httpUploadProgress', function (prog) {
+    s3uploader.on('httpUploadProgress', (prog) => {
       if (isAborted) return;
 
       uploader.progressLoaded = prog.loaded;
       uploader.emit('progress', uploader);
     });
-    s3uploader.send(function (err, data) {
+    s3uploader.send((err, data) => {
       if (isAborted) return;
 
       if (err) {
@@ -482,19 +317,15 @@ Client.prototype.downloadFile = function (params) {
   let localFile = params.localFile;
   let localFileSize = 0;
 
-  let parts = [];
-  let downloadedParts = params.downloadedParts || {};
   let isAborted = false;
-  let checkPoints = params.resumePoints;
   let isDebug = params.isDebug;
 
   let uploader = new EventEmitter();
   uploader.setMaxListeners(0);
   uploader.progressLoaded = 0;
-  uploader.progressParts = {};
   uploader.progressTotal = 0;
-  uploader.totalDownloadedParts = 0;
-  uploader.totalParts = 0;
+  uploader.progressPartSize = params.downloadedPartSize;
+  uploader.progressParts = params.downloadedParts || [];
   uploader.abort = handleAbort;
 
   params.s3Params.Key = encodeSpecialCharacters(params.s3Params.Key);
@@ -544,13 +375,13 @@ Client.prototype.downloadFile = function (params) {
 
   function startDownloadFile() {
     if (localFileSize >= self.multipartDownloadThreshold) {
-      if (self.resumeDownload) {
+      if (self.resumeDownload && self.multipartDownloadSize === uploader.progressPartSize) {
         resumeMultipartDownload();
       } else {
         startMultipartDownload();
       }
     } else {
-      doWithRetry(tryGettingObject, self.s3RetryCount, self.s3RetryDelay, function (err, data) {
+      tryGettingObject((err, data) => {
         if (isAborted) return;
 
         if (err) {
@@ -566,7 +397,7 @@ Client.prototype.downloadFile = function (params) {
   function startMultipartDownload() {
     if (isAborted) return;
 
-    fs.open(localFile, 'w+', function (err, fd) {
+    fs.open(localFile, 'w+', (err, fd) => {
       if (isAborted) return;
 
       if (err) {
@@ -579,45 +410,57 @@ Client.prototype.downloadFile = function (params) {
         Key: s3params.Key,
       };
 
-      makeMultipartDownload(fd);
+      tryMultipartDownload(fd, (err) => {
+        if (fd) {
+          fs.closeSync(fd);
+          fd = null;
+        }
+
+        if (isAborted) return;
+
+        if (err) {
+          handleError(err);
+          return;
+        }
+
+        uploader.emit('fileDownloaded');
+      });
     });
+
+    function tryMultipartDownload(fd, cb) {
+      if (isAborted) {
+        cb();
+        return;
+      }
+
+      let cursor = 0;
+      let nextPartNumber = 1;
+      while (cursor < uploader.progressTotal) {
+        let start = cursor;
+        let end = cursor + self.multipartDownloadSize;
+        if (end > uploader.progressTotal) {
+          end = uploader.progressTotal;
+        }
+        cursor = end;
+
+        let part = {
+          Done: false,
+          Start: start,
+          End: end,
+          PartNumber: nextPartNumber++
+        };
+
+        self.s3pend.go(startDownloadPart(fd, part));
+      }
+
+      self.s3pend.wait(cb);
+    }
   }
 
   function resumeMultipartDownload() {
+    if (isAborted) return;
 
-  }
-
-  function makeMultipartDownload(fd) {
-    let cursor = 0;
-    let nextPartNumber = 1;
-    while (cursor < uploader.progressTotal) {
-      let start = cursor;
-      let end = cursor + self.multipartDownloadSize;
-      if (end > uploader.progressTotal) {
-        end = uploader.progressTotal;
-      }
-      cursor = end;
-
-      let part = {
-        Done: false,
-        Start: start,
-        End: end,
-        PartNumber: nextPartNumber++
-      };
-
-      parts.push(part);
-
-      uploader.totalParts += 1;
-
-      self.s3pend.go(startDownloadPart(fd, part));
-    }
-
-    self.s3pend.wait(function (err) {
-      if (fd) {
-        fs.closeSync(fd);
-        fd = null;
-      }
-
+    fs.open(localFile, 'w+', (err, fd) => {
       if (isAborted) return;
 
       if (err) {
@@ -625,15 +468,72 @@ Client.prototype.downloadFile = function (params) {
         return;
       }
 
-      uploader.emit('fileDownloaded');
+      s3params = {
+        Bucket: s3params.Bucket,
+        Key: s3params.Key,
+      };
+
+      tryResumeMultipartDownload(fd, (err) => {
+        if (fd) {
+          fs.closeSync(fd);
+          fd = null;
+        }
+
+        if (isAborted) return;
+
+        if (err) {
+          handleError(err);
+          return;
+        }
+
+        uploader.emit('fileDownloaded');
+      });
     });
+
+    function tryResumeMultipartDownload(fd, cb) {
+      if (isAborted) {
+        cb();
+        return;
+      }
+
+      angular.forEach(uploader.progressParts, (part, partNumber) => {
+        if (part.Done === true) {
+          uploader.progressLoaded += self.multipartDownloadSize;
+        }
+      });
+      uploader.emit("progress", uploader);
+
+      let cursor = 0;
+      let nextPartNumber = 1;
+      while (cursor < uploader.progressTotal) {
+        let start = cursor;
+        let end = cursor + self.multipartDownloadSize;
+        if (end > uploader.progressTotal) {
+          end = uploader.progressTotal;
+        }
+        cursor = end;
+
+        // already done loaded
+        if (!uploader.progressParts[nextPartNumber] ||
+          uploader.progressParts[nextPartNumber].Done !== true) {
+          let part = {
+            Done: false,
+            Start: start,
+            End: end,
+            PartNumber: nextPartNumber++
+          };
+
+          self.s3pend.go(startDownloadPart(fd, part));
+        }
+      }
+
+      self.s3pend.wait(cb);
+    }
   }
 
   function startDownloadPart(fd, part) {
-    let retryCount = 0;
-
     return function (pendCb) {
-      doWithRetry(tryDownloadPart, self.s3RetryCount, self.s3RetryDelay, function (err, data) {
+      tryDownloadPart((err, data) => {
         pendCb();
 
         if (isAborted) return;
@@ -643,28 +543,25 @@ Client.prototype.downloadFile = function (params) {
           return;
         }
 
+        uploader.progressParts[part.PartNumber] = part;
         uploader.emit('filePartDownloaded', part);
       });
     };
 
     function tryDownloadPart(cb) {
-      retryCount++;
-
       if (isAborted) {
-        if (retryCount >= self.s3RetryCount) {
-          cb();
-        }
+        cb();
         return;
       }
 
-      if (!!downloadedParts[part.PartNumber]) {
-        part.Done = true;
-
+      if (part.Done === true) {
         cb(null, part);
         return;
       }
 
-      let params = {};
+      let params = Object.assign({}, s3params);
+      params.Range = `bytes=${part.Start}-${part.End}`;
+
       let partStream = fs.createWriteStream(null, {
         fd: fd,
         flags: 'r+',
@@ -672,41 +569,35 @@ Client.prototype.downloadFile = function (params) {
         autoClose: false
       });
 
-      Object.assign(params, s3params);
-      params.Range = `bytes=${part.Start}-${part.End}`;
-
       let s3downloader = self.s3.getObject(params).createReadStream();
       s3downloader.on('data', (chunk) => {
         if (isAborted) return;
 
-        uploader.progressParts[part.PartNumber] += chunk.length;
         uploader.progressLoaded += chunk.length;
-
         uploader.emit('progress', uploader);
       });
       s3downloader.on('end', () => {
+        if (isAborted) return;
+
         part.Done = true;
 
-        downloadedParts[part.PartNumber] = part;
-
-        uploader.totalDownloadedParts += 1;
         uploader.emit('progress', uploader);
 
-        cb(null);
+        cb(null, part);
       });
       s3downloader.on('error', (err) => {
         if (isAborted) return;
 
         cb(err);
       });
-      s3downloader.pipe(partStream);
+      s3downloader.pipe(self.s3stream).pipe(partStream);
     }
   }
 
   function tryGettingObject(cb) {
     let fileStream = fs.createWriteStream(localFile, {
       flags: 'w+',
-      autoClose: true
+      autoClose: false
     });
 
     let s3downloader = self.s3.getObject(s3params).createReadStream();
@@ -729,34 +620,10 @@ Client.prototype.downloadFile = function (params) {
 
       cb(err);
     });
-    s3downloader.pipe(fileStream);
+
+    s3downloader.pipe(self.s3stream).pipe(fileStream);
   }
 };
-
-function doWithRetry(fn, tryCount, delay, cb) {
-  var tryIndex = 0;
-
-  tryOnce();
-
-  function tryOnce() {
-    fn(function (err, result) {
-      if (err) {
-        if (err.retryable === false) {
-          cb(err);
-        } else {
-          tryIndex += 1;
-          if (tryIndex >= tryCount) {
-            cb(err);
-          } else {
-            setTimeout(tryOnce, delay);
-          }
-        }
-      } else {
-        cb(null, result);
-      }
-    });
-  }
-}
 
 function encodeSpecialCharacters(filename) {
   // Note: these characters are valid in URIs, but S3 does not like them for

6b1be36560786dfd6013aefb516fe839648a5389
Bachue Zhou <bachue.shu@gmail.com>
2020-09-26 13:24:29 +0800

    now drag zone is not yellow anymore

--- app/components/directives/drop-zone.js
+++ app/components/directives/drop-zone.js
@@ -21,30 +21,8 @@ angular
         e.originalEvent.preventDefault();
       }
 
-      var shadow;
-      $(ele).on("dragenter", function () {
-        shadow = $("<div></div>")
-          .css({
-            position: "absolute",
-            height: $(ele).height(),
-            width: $(ele).width(),
-            opacity: 0.5,
-            top: $(ele).offset().top,
-            left: $(ele).offset().left,
-            background: "yellow",
-            zIndex: 20,
-            boxShadow: "inset yellow 0 0 10px"
-          })
-          .appendTo("body");
-
-        shadow
-          .on("dragleave", function () {
-            shadow.remove();
-          })
-          .on("drop", function (e) {
-            shadow.remove();
-            scope.dropZone(e);
-          });
+      $(ele).on("drop", function(e) {
+        scope.dropZone(e);
       });
     }
   });

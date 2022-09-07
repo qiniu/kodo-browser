bfcbe5b41c671b6d8ab593e638f26448ff77acd9
LiHS <lihsai0@gmail.com>
2022-09-07 17:20:28 +0800

    modify component top to display content by auth state

--- src/renderer/components/top/menu-contents.tsx
+++ src/renderer/components/top/menu-contents.tsx
@@ -1,31 +1,41 @@
 import {Form, Nav} from "react-bootstrap";
 import {LangName, useI18n} from "@renderer/modules/i18n";
 import React from "react";
+import {useAuth} from "@renderer/modules/auth";
 
 const MenuContents: React.FC = () => {
   const {translate, setLanguage} = useI18n();
+  const {currentUser} = useAuth();
+  if (currentUser) {
+    return (
+      <Nav className="flex-grow-1">
+        <Nav.Link>
+          <i className="bi bi-folder2 me-2"/>
+          {translate("top.files")}
+        </Nav.Link>
+        <Nav.Link>
+          <i className="bi bi-star-fill me-2"/>
+          {translate("top.bookmarks")}
+        </Nav.Link>
+        <Nav.Link>
+          <i className="bi bi-gear-fill me-2"/>
+          {translate("top.settings")}
+        </Nav.Link>
+        <Nav.Link>
+          <i className="bi bi-info-lg"/>
+          {translate("top.about")}
+        </Nav.Link>
+      </Nav>
+    );
+  }
 
   return (
     <>
       <Nav className="flex-grow-1">
-        <Nav className="flex-grow-1">
-          <Nav.Link>
-            <i className="bi bi-folder2 me-2"/>
-            {translate("top.files")}
-          </Nav.Link>
-          <Nav.Link>
-            <i className="bi bi-star-fill me-2"/>
-            {translate("top.bookmarks")}
-          </Nav.Link>
-          <Nav.Link>
-            <i className="bi bi-gear-fill me-2"/>
-            {translate("top.settings")}
-          </Nav.Link>
-          <Nav.Link>
-            <i className="bi bi-info-lg"/>
-            {translate("top.about")}
-          </Nav.Link>
-        </Nav>
+        <Nav.Link>
+          <i className="bi bi-info-lg"/>
+          {translate("top.about")}
+        </Nav.Link>
       </Nav>
       <Form className="d-flex flex-grow-0 align-items-center">
         <div className="text-light flex-shrink-0 pe-1">{translate("top.language")}</div>
